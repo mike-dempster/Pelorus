b817132c614d6dad9339fe1be626766f226801c3
mike-dempster <michael.j.dempster@gmail.com>
2015-01-03 10:14:19 -0700

    Added common validators and type conversion helpers.

---
+++ Pelorus.Core/Types.cs
@@ -0,0 +1,41 @@
+﻿using System.ComponentModel;
+
+namespace Pelorus.Core
+{
+    /// <summary>
+    /// Helper methods for casting objects to and from different types.
+    /// </summary>
+    public static class Types
+    {
+        /// <summary>
+        /// Convert the given object to type 'T'.  If the object cannot be cast to type T then return the default value of T.
+        /// </summary>
+        /// <typeparam name="T">Type to cast object to.</typeparam>
+        /// <param name="subject">Object to cast.</param>
+        /// <returns>Subject cast to type T or the default value ot type T if a converter does not exist.</returns>
+        public static T Cast<T>(object subject)
+        {
+            if (null == subject)
+            {
+                return default(T);
+            }
+
+            var subjectType = typeof(T);
+            var converter = TypeDescriptor.GetConverter(subjectType);
+
+            if (converter.CanConvertFrom(subjectType))
+            {
+                return (T)converter.ConvertFrom(subject);
+            }
+
+            converter = TypeDescriptor.GetConverter(subject);
+
+            if (converter.CanConvertTo(subjectType))
+            {
+                return (T)converter.ConvertTo(subject, subjectType);
+            }
+
+            return default(T);
+        }
+    }
+}
---
+++ Pelorus.Core/Validation/CommonValidators.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Globalization;
+using System.Text.RegularExpressions;
+
+namespace Pelorus.Core.Validation
+{
+    /// <summary>
+    /// Validators for common string types.
+    /// </summary>
+    public static class CommonValidators
+    {
+        private static Regex emailRegex = new Regex(@"^(?("")(""[^""]+?""@)|(([0-9a-zA-Z]((\.(?!\.))|[-!#\$%&'\*\+/=\?\^`\{\}\|~\w])*)(?<=[0-9a-zA-Z])@))(?(\[)(\[(\d{1,3}\.){3}\d{1,3}\])|(([0-9a-zA-Z][-\w]*[0-9a-zA-Z]*\.)+[0-9a-zA-Z]{2,24}))$");
+        private static Regex phoneNumberRegex = new Regex(@"[\(]{0,1}[0-9]{3}[\s]{0,}[-\.\*\)]{0,1}[\s]{0,}[0-9]{3}[\s]{0,}[-\.\*\)]{0,1}[\s]{0,}[0-9]{4}");
+        private static Regex zipCodeRegex = new Regex(@"[0-9]{5}(-[0-9]{4}){0,1}");
+
+        /// <summary>
+        /// Validate an email address.
+        /// </summary>
+        /// <param name="emailAddress">String to validate as an email address.</param>
+        public static void ValidateEmailAddress(string emailAddress)
+        {
+            if (string.IsNullOrWhiteSpace(emailAddress))
+            {
+                throw new ArgumentNullException("emailAddress");
+            }
+
+            emailAddress = emailAddress.ToLower()
+                                       .Trim();
+            var matches = emailRegex.Matches(emailAddress);
+
+            if ((1 != matches.Count) || ((0 < matches.Count) && (!matches[0].Value.Equals(emailAddress))))
+            {
+                throw new Exception(string.Format(CultureInfo.InvariantCulture, "Invalid email address: {0}", emailAddress));
+            }
+        }
+
+        /// <summary>
+        /// Validate a US phone number.
+        /// </summary>
+        /// <param name="phoneNumber">String to validate as a phone number.</param>
+        public static void ValidatePhoneNumber(string phoneNumber)
+        {
+            if (string.IsNullOrWhiteSpace(phoneNumber))
+            {
+                throw new ArgumentNullException("phoneNumber");
+            }
+
+            var matches = phoneNumberRegex.Matches(Types.Cast<string>(phoneNumber));
+
+            if ((1 != matches.Count) || ((0 < matches.Count) && (!matches[0].Value.Equals(phoneNumber))))
+            {
+                throw new Exception(string.Format("Invalid phone number: {0}", phoneNumber));
+            }
+        }
+
+        /// <summary>
+        /// Validate a US postal code.
+        /// </summary>
+        /// <param name="zipCode">String to validate as a US postal code.</param>
+        public static void ValidateZipCode(string zipCode)
+        {
+            if (string.IsNullOrWhiteSpace(zipCode))
+            {
+                throw new ArgumentNullException("zipCode");
+            }
+
+            MatchCollection matches = zipCodeRegex.Matches(Types.Cast<string>(zipCode));
+
+            if ((1 != matches.Count) || ((0 < matches.Count) && (!matches[0].Value.Equals(zipCode))))
+            {
+                throw new Exception(string.Format("Invalid zip code: {0}", zipCode));
+            }
+        }
+    }
+}
